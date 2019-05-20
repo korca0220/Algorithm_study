18f19da04fb507d1ec2af6073f8b716663529571
korca0220 <korca02220@naver.com>
2019-05-21 00:16:41 +0900

    집저장

--- ACMICPC/SWtest/Basic/Math/6588.cpp
+++ ACMICPC/SWtest/Basic/Math/6588.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 using namespace std;
 
 const int MAX = 1000000;
 bool isPrime[MAX];
-
+vector<int> odd;
 
 void getPrime(){
 
@@ -15,12 +16,26 @@ void getPrime(){
         }
     }
 
+    for(int i=2; i<=MAX; i++){
+        if(i%2 != 0 && isPrime[i])odd.push_back(i);
+    }
+
 }
 
 
 int main(){
 
+    cin.sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     getPrime();
+
+    int input=-1;
+    while(input == 0){
+        cin >> input;
+        
+    }
 
     return 0;
 }
