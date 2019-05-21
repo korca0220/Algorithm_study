f43d41af3b1f3d06a03d971f3e6987a229d640d4
ParkJuneWoo <korca02220@naver.com>
2019-05-21 11:36:17 +0900

    6588 solved

--- ACMICPC/SWtest/Basic/Math/6588.cpp
+++ ACMICPC/SWtest/Basic/Math/6588.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <cstdio>
 #include <cmath>
 #include <vector>
 using namespace std;
 
 const int MAX = 1000000;
 bool isPrime[MAX];
-vector<int> odd;
 
-void getPrime(){
+
+vector<int> getPrime(){
+
+    vector<int> odd_number;
+    odd_number.push_back(0); // [0]
+    odd_number.push_back(0); // [1]
 
     for(int i=2; i<=MAX; i++)isPrime[i] = true;
     for(int i=2; i<=sqrt(MAX); i++){
@@ -17,24 +22,41 @@ void getPrime(){
     }
 
     for(int i=2; i<=MAX; i++){
-        if(i%2 != 0 && isPrime[i])odd.push_back(i);
+        if(i%2 != 0 && isPrime[i])odd_number.push_back(i);
     }
-
+    
+    return odd_number;
 }
 
 
 int main(){
 
-    cin.sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    
+    vector<int> odd = getPrime();
+    int input=1;
 
-    getPrime();
-
-    int input=-1;
-    while(input == 0){
-        cin >> input;
+    while(true){
         
+        scanf("%d", &input);
+        if(input==0)break;                             
+        bool check=false;
+        int a,b;
+        int max = 0;        
+
+        for(int i=2; i<odd.size(); i++){
+            for(int j=i; j<odd.size(); j++){                     
+                if(odd[i] + odd[j] == input){                                                                                   
+                    a = odd[i];
+                    b = odd[j];
+                    check = true;
+                    break;                
+                }
+                else if(odd[i] + odd[j] > input)break;
+            }
+            if(check)break;        
+        }
+        if(!check) printf("Goldbach's conjecture is wrong.\n");
+        else printf("%d = %d + %d\n", input, a, b);
     }
 
     return 0;
