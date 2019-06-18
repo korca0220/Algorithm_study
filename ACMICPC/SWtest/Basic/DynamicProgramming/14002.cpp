2f2eb2be96e4c8297847058244a75d787c29877f
Junewoo <korca02220@naver.com>
2019-06-18 18:14:33 +0900

    14002_solved(ref)

---
+++ ACMICPC/SWtest/Basic/DynamicProgramming/14002.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int input;
+int dp[1001];
+int in[1001];
+int v[10001];
+int p;
+
+void printDp(int n){
+
+    if(n == 0)return ;
+    printDp(v[n]);
+    cout << in[n] << " ";
+}
+
+int getDp(){
+
+    for(int i = 1; i<=input; i++){
+        dp[i] = 1;
+        v[i] = 0;
+        for(int j=1; j<i; j++){
+            if(in[i] > in[j] && dp[i] < dp[j]+1){
+                dp[i] = dp[i] + 1;
+                v[i] = j;
+            }
+        }
+    }
+
+    int max_number=0;
+    for(int i=1; i<=input; i++){
+        if(max_number < dp[i]){
+            max_number = dp[i];
+            p = i;
+        }
+    }
+
+    return max_number;
+}
+
+
+int main(){
+
+    cin.sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> input;
+    for(int i=1; i<=input; i++){
+        cin >> in[i];
+    }
+    cout << getDp() << "\n";
+    printDp(p);
+
+    return 0;
+}
