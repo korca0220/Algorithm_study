c383a3c8dfbb1c3e7f34b486458149c882be4165
ParkJuneWoo <korca02220@naver.com>
2019-06-18 17:19:00 +0900

    11053_solved

---
+++ ACMICPC/SWtest/Basic/DynamicProgramming/11053.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int input;
+int dp[1001];
+int in[1001];
+
+int getDp(){
+
+    for(int i =1; i<=input; i++){
+        dp[i] = 1;
+        for(int j=1; j<i; j++){
+            if(in[i] > in[j] && dp[i] < dp[j]+1){
+                dp[i] = dp[i] + 1;
+                
+            }
+        }
+    }
+
+    int max_number=0;
+    for(int i=1; i<=input; i++){
+        max_number = max(max_number, dp[i]);
+    }
+
+    return max_number;
+}
+
+
+int main(){
+
+    cin.sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> input;
+    for(int i=1; i<=input; i++){
+        cin >> in[i];
+    }
+    cout << getDp() << "\n";
+
+    return 0;
+}
