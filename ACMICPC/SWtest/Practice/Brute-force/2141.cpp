1ecd5fbefb0e9cce65ff7b90870014cb240529e8
Notebook-june <korca02220@naver.com>
2019-07-08 23:41:49 +0900

    2141_solved(ref_new_method)

--- ACMICPC/SWtest/Practice/Brute-force/2141.cpp
+++ ACMICPC/SWtest/Practice/Brute-force/2141.cpp
@@ -10,55 +10,39 @@ int main(){
     
     scanf("%d", &n);
     vector<int> v_n(n);
-    vector<int> rv_n(1<<n);
+    vector<int> rv_n;
     for(int i=0; i<n; i++)scanf("%d", &v_n[i]);
 
-    for(int i=0; i<(1<<n); i++){
-        for(int j=0; j<n; j++){
-            if(i&(1<<j))rv_n[i] += v_n[j];
+    for(int i=0; i<n; i++){
+        int sum =0;
+        for(int j=i; j<n; j++){
+            sum += v_n[j];
+            rv_n.push_back(sum);
         }
     }
 
 
     scanf("%d", &m);
     vector<int> v_m(m);
-    vector<int> rv_m(1<<m);
+    vector<int> rv_m;
     for(int i=0; i<m; i++)scanf("%d", &v_m[i]);
     
-    for(int i=0; i<(1<<m); i++){
-        for(int j=0; j<m; j++){
-            if(i&(1<<j)) rv_m[i] += v_m[j];
+    for(int i=0; i<m; i++){
+        int sum = 0;
+        for(int j=i; j<m; j++){
+            sum += v_m[j];
+            rv_m.push_back(sum);
         }
     }
     sort(rv_n.begin(), rv_n.end());
     sort(rv_m.begin(), rv_m.end());
-    reverse(rv_m.begin(), rv_m.end());
-    for(int i=0; i<rv_n.size(); i++)printf("%d ", rv_n[i]);
-    printf("\n");
-    for(int i=0; i<rv_m.size(); i++)printf("%d ", rv_m[i]);
-    int left=0, right=0;
     long long ans=0;
-    n = (1<<n);
-    m = (1<<m);
-    while(left < n && right < m){
-        if(rv_n[left] + rv_m[right] == T){
-            long long l = 1;
-            long long r = 1;
-            left+=1;
-            right+=1;
-            while(left < n && rv_n[left]==rv_n[left-1]){
-                l+=1;
-                left+=1;
-            }
-            while(right < m && rv_m[right]==rv_m[right-1]){
-                r=+1;
-                right+=1;
-            }
-            ans += l * r;
-        }else if(rv_n[left] + rv_m[right] < T){
-            left+=1;
-        }else right +=1;
+
+    for(int i=0; i<rv_n.size(); i++){
+        auto p = equal_range(rv_m.begin(), rv_m.end(), T-rv_n[i]);
+        ans += p.second - p.first;
     }
+    
     printf("%lld", ans);
 
 
