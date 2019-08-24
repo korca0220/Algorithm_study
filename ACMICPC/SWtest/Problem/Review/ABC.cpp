089cf4fb358c15e0d8b0940ca58c1dc29c6be0ac
korca0220 <korca02220@naver.com>
2019-08-24 15:41:18 +0900

    ABC_solved

---
+++ ACMICPC/SWtest/Problem/Review/ABC.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int dp[30][30][30][435];
+int n, k;
+char in[31];
+bool getChar(int i, int a, int b, int p){
+
+    if(i==n){
+        if(k==p) return true;
+        else return false;
+    }
+    if(dp[i][a][b][p] > 0 ) return false;
+
+    dp[i][a][b][p] = true;
+
+    in[i] = 'A';
+    if(getChar(i+1, a+1, b, p)) return true;
+
+    in[i] = 'B';
+    if(getChar(i+1, a, b+1, p+a)) return true;
+
+    in[i] = 'C';
+    if(getChar(i+1, a, b, p+a+b)) return true;
+
+    return false;
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> k;
+    if(getChar(0,0,0,0)){
+        cout << in;
+    }else cout << "-1";
+
+    return 0;
+}
