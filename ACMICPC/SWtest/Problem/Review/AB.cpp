d40f51c4c546b4be7c13ab63117847adf7242fd4
korca0220 <korca02220@naver.com>
2019-08-29 21:40:02 +0900

    AB_solved(ref)

---
+++ ACMICPC/SWtest/Problem/Review/AB.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, k;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> k;
+
+    for(int a=0; a<=n; a++){
+        int b = n-a;
+        if(a*b < k) continue;
+        vector<int> cnt(b+1);
+        for(int i=0; i<a; i++){
+            int x = min(b, k);
+            cnt[x] +=1;
+            k -= x;
+        }
+
+        for(int i=b; i>=0; i--){
+            for(int j=0; j<cnt[i]; j++){
+                cout << "A";
+            }
+            if(i>0) cout << "B";
+        }
+        return 0;
+    }
+    cout << "-1";
+
+    return 0;
+}
