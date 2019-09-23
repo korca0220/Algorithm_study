685145822d21c4b8a2a6d77a1cc0346762aeb78c
korca0220 <korca02220@naver.com>
2019-09-24 02:14:39 +0900

    modifyt

--- Programmers/Level2/15.cpp
+++ Programmers/Level2/15.cpp
@@ -17,24 +17,24 @@ int solution(vector<vector<int>> jobs) {
     int answer = 0;
     priority_queue<pair<int,int>, vector<pair<int,int>>, comp> pq;
     for(int i=0; i<jobs.size(); i++)pq.emplace(jobs[i][0], jobs[i][1]);
-    int start = 0;
-    int point = 0;
     
-    while(true){
-        for(; point<jobs.size(); point++){
-            if(jobs[point][0] <=point){
-                pq.emplace(jobs[point][0], jobs[point][1]);
+    vector<pair<int,int>> v;
+    for(int i=0; i<pq.size(); i++){
+        v.emplace_back(pq.top());
+        pq.pop();
+    }
+    
+    int time = 0;
+    while(!v.empty()){
+        for(int i=0; i<v.size(); i++){
+            if(v[i].first <= time){
+                time += v[i].second;
+                answer += time - v[i].first;
+                v.erase(v.begin()+i);
+                break;
             }
-            if(point == jobs.size()) break;
-            if(pq.empty()){
-                start+=1;
-                continue;
-            }
-            auto temp = pq.top(); pq.pop();
-            answer += start - temp.first + temp.second;
-            start += temp.second;
+            if(i == v.size() -1)time ++;
         }
-
     }
     return answer/jobs.size();
 }
