e138058ed8fe2bf918be538b1b346a7fb9d15ada
korca0220 <korca02220@naver.com>
2019-09-25 04:19:40 +0900

    sort

--- ETC/quick_sort.cpp
+++ ETC/quick_sort.cpp
@@ -24,10 +24,23 @@ void quickSort(int arr[], int start, int end){
         quickSort(arr, part, end);
     }
 }
+
+void bubbleSort(int arr[], int size){
+
+    int len = size;
+    for(int i=0; i<len-1; i++){
+        for(int j=0; j<len-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
 int main(){
 
     int arr[8] = {5,7,1,2,3,4,5,8};
-    quickSort(arr, 0, 7);
+    // quickSort(arr, 0, 7);
+    bubbleSort(arr, 8);
     for(int i : arr){
         cout << i << " ";
     }
